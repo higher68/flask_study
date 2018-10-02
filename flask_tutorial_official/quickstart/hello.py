b8ef18276106b2d84fd2464742b51bc388844906
junya <hatiro7code@gmail.com>
2018-10-02 21:05:10 +0900

    20181001 sample-decorator flask hello

--- flask_tutorial_official/quickstart/hello.py
+++ flask_tutorial_official/quickstart/hello.py
@@ -7,9 +7,42 @@ from flask import Flask  # Flaskクラスのインポート
 # アプリとして起動したときは、__name__の中身は__main__、モジュールとしてインポートされたときは、ファイル名となる
 app = Flask(__name__)  # インスタンスの生成 Flaskがどこからファイルを探すかを認識するのに必要らしいが。うーん￥？
 # この場合は、Flask.route()メソッドにデコレートしてる。
+# route関数の詳細は下。
+# 今回は、デコレートする時に、add_url_ruleが実行されてる。
+## def route(self, rule, **options):
+##     """A decorator that is used to register a view function for a
+##     given URL rule.  This does the same thing as :meth:`add_url_rule`
+##     but is intended for decorator usage::
+
+##         @app.route('/')
+##         def index():
+##             return 'Hello World'
+
+##     For more information refer to :ref:`url-route-registrations`.
+
+##     :param rule: the URL rule as string
+##     :param endpoint: the endpoint for the registered URL rule.  Flask
+##                      itself assumes the name of the view function as
+##                      endpoint
+##     :param options: the options to be forwarded to the underlying
+##                     :class:`~werkzeug.routing.Rule` object.  A change
+##                     to Werkzeug is handling of method options.  methods
+##                     is a list of methods this rule should be limited
+##                     to (``GET``, ``POST`` etc.).  By default a rule
+##                     just listens for ``GET`` (and implicitly ``HEAD``).
+##                     Starting with Flask 0.6, ``OPTIONS`` is implicitly
+##                     added and handled by the standard request handling.
+##     """
+    ## def decorator(f):
+    ##     endpoint = options.pop('endpoint', None)
+    ##     self.add_url_rule(rule, endpoint, f, **options)
+    ##     return f
+    ## return decorator
+
+
 @app.route('/')  # デコレータ。これを使うと関数を簡単に上書きできる
-def hello_world():
-    return "Hello World!"
+ def hello_world():
+     return "Hello World!"
 
 # def hello_world():
 #    return "Hello World!"
