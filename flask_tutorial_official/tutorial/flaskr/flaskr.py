5d842112f7cbfa59882a5f91528de711889bf0fa
junya <hatiro7code@gmail.com>
2018-10-11 20:22:08 +0900

    20181011 add init_db

--- flask_tutorial_official/tutorial/flaskr/flaskr.py
+++ flask_tutorial_official/tutorial/flaskr/flaskr.py
@@ -2,6 +2,11 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
                   abort, render_template, flash
+from __future__ import with statement
+from contextlib import closing  # ブロックの完了時にthingをcloseするコンテキストマネージャを返す。
+# 要するに、処理が完了したら閉じてくれるってことか
+# コンテキストマネージャーは__enter__()と__exit__()メソッドを実行する。
+# 自動でログイン・ログアウトしてくれる的な?
 
 # configuration
 DATABASE = "/tmp/flaskr.db"
@@ -24,6 +29,16 @@ def connect_db():
     return connectionオブジェクト。データベースを表すらしい
     '''
     return sqlite3.connect(app.config["DATABASE"])
+
+
+def init_db():
+    '''
+    データベースの初期化
+    '''
+    with closing(connecet_db()) as db:
+        with app.open_resource('schema.sql') as f:  # なんかよくわからんが、shema.sqlを実行してるのはわかるapplication objectらしい
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 if __name__ == "__main__":
