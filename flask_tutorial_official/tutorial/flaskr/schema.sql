4b7912f2c5d938bd556692cbceb8c0ff5a3dfeb6
junya <hatiro7code@gmail.com>
2018-10-14 14:48:22 +0900

    20181014 1st line bag fixed

--- flask_tutorial_official/tutorial/flaskr/schema.sql
+++ flask_tutorial_official/tutorial/flaskr/schema.sql
@@ -1,6 +1,8 @@
-drop table if exists entries:  -- entriesテーブルが存在していたら、dropつまり、テーブルを削除する。テーブルってまあ表みたいなやつ？
-create tabele entries { -- cretate tableでテーブルを作成できる。
+--sqlite3 /tmp/flaskr.db < schema.sqlはファイル内に書いちゃダメだよ。sql的に変--
+--near "sqlite3": syntax error--
+drop table if exists entries;  -- entriesテーブルが存在していたら、dropつまり、テーブルを削除する。テーブルってまあ表みたいなやつ？
+create table entries (-- cretate tableでテーブルを作成できる。
        id integer primary key autoincrement,  --column1、データ型integere、primary key指定すると、主キーになる。
        title string not null, -- nullを入れられないってことか
        text string not null
-};
+);
