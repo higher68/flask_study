7e0f5b662ab5065292d789f0beef20f17bfb497c
junya <hatiro7code@gmail.com>
2018-10-23 21:08:32 +0900

    rendering added

--- web_service/flaskApp/app.py
+++ web_service/flaskApp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template  # 特定のルーティングからテンプレートエンジンを呼び出す
 # Flaskクラスのインポート
 app = Flask(__name__)  # アプリの本体をインスタンスとして作成
 # webアプリでは、http://のあとの最初の/で、ユーザーのリクエストの種類を見分けている。
@@ -8,7 +9,13 @@ app = Flask(__name__)  # アプリの本体をインスタンスとして作成
 # defaultでは、Flaskのアプリはlocalhost:5000でリクエストを待ってる
 # @app.route('/')なら、localhost:5000にアクセスした時のレスポンス
 # @app.route('/hoge')なら、localhost:5000/hogeにアクセスした時のレスポンスを指定
+app = Flask(__name__)
 
+# 下記、routing部分の<title>は、x:/title/<title>へのアクセス、o:<title>の位置に入力された文字列をルーティング関数内で使えるようにする処理。要するに、title=input()ってこと？
+# <title>で受け取った値はtitle関数の中で同じ名前の引数として渡される。
+@app.route('/title/<title>')
+def title(title):
+    return render_template('index.html', title=title)
 
 # /にアクセスがあった時の処理
 @app.route('/')
