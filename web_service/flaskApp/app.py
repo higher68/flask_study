3f294e209f9b8bc3a30a43160914cccd0bd03582
junya <hatiro7code@gmail.com>
2018-10-23 20:53:32 +0900

    debug, html&stylesheet added

--- web_service/flaskApp/app.py
+++ web_service/flaskApp/app.py
@@ -18,13 +18,13 @@ def hello_world():
 <html>
 <head>
 <meta charset="utf-8">
-<link = "stylesheet"
-href-"/static/style.css"/>
+<link rel="stylesheet"
+href="/static/style.css"/>
 </head>
 <body>
 <h1> Hello World</h1>
 </body>
-</html
+</html>
 """
 
 
