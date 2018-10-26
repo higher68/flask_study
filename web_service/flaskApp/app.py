82ce092ddbda5d8c9c70ca750f0a1c651dcb9f11
junya <hatiro7code@gmail.com>
2018-10-26 13:33:26 +0900

    20181026 autoscape banned

--- web_service/flaskApp/app.py
+++ web_service/flaskApp/app.py
@@ -74,8 +74,11 @@ def upload_file():
 # /にアクセスがあった時の処理
 @app.route('/')
 def index():
-    # 辞書データにアクセス
-    return render_template('index.html', obj={"title": "hoge"})
+    # scriptタグがそのまま出力されてしまい危険
+    return render_template('index.html', unescaped="<script>alert('hoge')</script>")
+## def index():
+##     # 辞書データにアクセス
+##     return render_template('index.html', obj={"title": "hoge"})
 ## def index():
 ##     # 単純に引数をテンプレートに渡す
 ##     return render_template('index.html', title="Hello World")
