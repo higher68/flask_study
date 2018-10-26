525dff8f827e392201b22e66f65675eb12571bca
junya <hatiro7code@gmail.com>
2018-10-26 18:52:01 +0900

    20181026 for loop index to table added

--- web_service/flaskApp/app.py
+++ web_service/flaskApp/app.py
@@ -20,77 +20,82 @@ app = Flask(__name__)
 def title(title):
     return render_template('index.html', title=title)
 
-# ルーティングからユーザがパスに入力した値を利用する例
-@app.route('/user/<string:username>')  # stringに引数の型を指定
-def user(username):
-    return username
+# ルーティングからユーザがパスに## 入力した値を利用する例
+## @app.route('/user/<string:username>')  # stringに引数の型を指定
+## def user(username):
+##     return username
 
 
-@app.route('/user/<int:user_id>')
-def user_id(id):
-    return id
+## @app.route('/user/<int:user_id>')
+## def user_id(id):
+##     return id
 
 
-@app.route('/search')
-def search():
-    '''
-qで指定されたパラメータを取得
-URLのパラメータを取得
-'''
-    q = request.args.get('q', '')
-    return q  # ?q=何ちゃらの何ちゃらが出力される。returnなのに注意。htmlは介してない。
+## @app.route('/search')
+## def search():
+##     '''
+## qで指定されたパラメータを取得
+## URLのパラメータを取得
+## '''
+##     q = request.args.get('q', '')
+##     return q  # ?q=何ちゃらの何ちゃらが出力される。returnなのに注意。htmlは介してない。
 
-@app.route('/login', methods=['GET'])
-def render_form():
-    return render_template('login.html')
+## @app.route('/login', methods=['GET'])
+## def render_form():
+##     return render_template('login.html')
 
-# request.formってもしや、formタグ中のinputの中のnameに格納された変数を指定すると受け取れるってことかな
-@app.route('/login', methods=['POST'])
-def login():
-    print("hogerun")
-    if request.form['username'] and request.form['email']:
-        return render_template('check.html', email=request.form['email'], username=request.form['username'])
-    else:
-        return render_template('error.html')
+## # request.formってもしや、formタグ中のinputの中のnameに格納された変数を指定すると受け取れるってことかな
+## @app.route('/login', methods=['POST'])
+## def login():
+##     print("hogerun")
+##     if request.form['username'] and request.form['email']:
+##         return render_template('check.html', email=request.form['email'], username=request.form['username'])
+##     else:
+##         return render_template('error.html')
     
-@app.route('/upload', methods=['GET'])
-def render_upload_form():
-    return render_template('upload.html')
+## @app.route('/upload', methods=['GET'])
+## def render_upload_form():
+##     return render_template('upload.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.form['name'] and request.files['image']:
-        print("hoge")
-        f = request.files['image']
-        filepath = 'static/' + secure_filename(f.filename)
-        # secure_filenameは例外辞書に当てはまるものを弾く仕様なのかな
-        print(filepath)
-        f.save(filepath)
-        return render_template('result.html', name=request.form['name'], image_url=filepath)
+## @app.route('/upload', methods=['POST'])
+## def upload_file():
+##     if request.form['name'] and request.files['image']:
+##         print("hoge")
+##         f = request.files['image']
+##         filepath = 'static/' + secure_filename(f.filename)
+##         # secure_filenameは例外辞書に当てはまるものを弾く仕様なのかな
+##         print(filepath)
+##         f.save(filepath)
+##         return render_template('result.html', name=request.form['name'], image_url=filepath)
 
 
 
-# /にアクセスがあった時の処理
-@app.route('/')
-## def index():
-##     # scriptタグがそのまま出力されてしまい危険
-##     return render_template('index.html', unescaped="<script>alert('hoge')</script>")
-## def index():
-##     # 辞書データにアクセス
-##     return render_template('index.html', obj={"title": "hoge"})
-## def index():
-##      # 単純に引数をテンプレートに渡す
-##      return render_template('index.html', title="Hello World")
-# randomで表示ページを変える。
+## # /にアクセスがあった時の処理
+## @app.route('/')
+## ## def index():
+## ##     # scriptタグがそのまま出力されてしまい危険
+## ##     return render_template('index.html', unescaped="<script>alert('hoge')</script>")
+## ## def index():
+## ##     # 辞書データにアクセス
+## ##     return render_template('index.html', obj={"title": "hoge"})
+## ## def index():
+## ##      # 単純に引数をテンプレートに渡す
+## ##      return render_template('index.html', title="Hello World")
+## # randomで表示ページを変える。
+## ## @app.route('/')
+## ## def index():
+## ##     return render_template('index.html', random=random())
+## # loopでリスト表示
 ## @app.route('/')
 ## def index():
-##     return render_template('index.html', random=random())
-# loopでリスト表示
+##     return render_template('index.html', l=["Hoge", "Fuga", "Foo"])
+# tableへの辞書型を用いたループ処理
 @app.route('/')
 def index():
-    return render_template('index.html', l=["Hoge", "Fuga", "Foo"])
-## ef hello_world():  # render_templateを使わない時
+    return render_template('index.html', l=[{"name":"Hoge", "value":"1"}, {"name":"Fuga","value":"2"}, {"name":"Foo", "value":"3" }])
+
+## def hello_world():  # render_templateを使わない時
 ##     return """
 ## <!DOCTYPE>
 ## <html>
