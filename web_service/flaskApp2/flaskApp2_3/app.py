383603c8d613b878fa65af63a57eea57ffd1c9b1
junya <hatiro7code@gmail.com>
2018-11-04 16:27:27 +0900

    20181103 bag fixed problem=need initdb

--- web_service/flaskApp2/flaskApp2_3/app.py
+++ web_service/flaskApp2/flaskApp2_3/app.py
@@ -4,10 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/hestdv"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # ちょっと意味わからんdefaultはfalseらしい
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/testdb"  # dbのURI.データベースにアクセスするために必要
 db = SQLAlchemy(app)
 # ORMで言語のクラスとテーブルを結びつけるためのモデル
+# 最初のdb.Columnで、テーブルの中身を設定してる。
 class User(db.Model):  # db.Modelを継承
     id = db.Column(db.Integer, primary_key=True)  # ユーザをデータベース、アプリ内部で区別するための固有番号
     # 第一引数がデータ構造。これに合わないものが挿入されるとエラーを吐く, primary_keyはデータを挿入した順に通し番号が振られるってこと。
@@ -16,7 +17,7 @@ class User(db.Model):  # db.Modelを継承
     description = db.Column(db.String(120), index=True, unique=True)
     user_image_url = db.Column(db.String(120), index=True, unique=True)
     def __repr__(self):
-        return '<User %r>'% self.username
+        return '<User %r>' % self.username  # instanceの出力。って何？
 
 @app.route('/')
 def index():
@@ -26,27 +27,31 @@ def index():
 
 
 @app.route('/form')
-def forn():
+def form():
     return render_template("form.html")
 
 
 @app.route("/register", methods=["POST"])
 def register():
-    if request.form["username"] and request.form["description"] and request.files["image"]:
-        f = request.fils["image"]
+    print('hogeta')
+    if request.form['username'] and request.form['description'] and request.files['image']:
+        # print("hogeo")
+        f = request.files["image"]
         filepath = "static/" + secure_filename(f.filename)
         f.save(filepath)
-
-
         filepath= "/" + filepath
+        # print("geeeee")
         newUser = User(username=request.form["username"], description=request.form["description"], user_image_url=filepath)
         db.session.add(newUser)
+        # print('hooooo',newUser.username, newUser.description, newUser.user_image_url)
+        # print(newUser)
         db.session.commit()
+        # print("ararara")
         return render_template("result.html", username=request.form["username"], description=request.form["description"])
     else:
         return render_template("error_html")
 
 
-@app.cli.command("initdb")
+@app.cli.command("initdb")  # 高設定するとflask initdbでメソッドが使える
 def initdb_command():
-    db.create_all()
+    db.create_all()  # これを行うと、モデルからデータベースの操作を行うことができるようになる。必ずやる
